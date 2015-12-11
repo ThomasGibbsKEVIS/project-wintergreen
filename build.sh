43ba25e84481a9d686419282ebca5f3a3cd2e26f
0xF3d04a <0xF3d04a@users.noreply.github.com>
2015-12-11 13:32:06 -0500

    Added instructions for bug fixes and the kernel

--- build.sh
+++ build.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+if [ -d build ] then
+  echo -n -e "\e[37mCreating build directory..."
+  mkdir build
+  echo -e "\e[32mDone!"
+fi
 echo -n -e "\e[37mCompiling binaries..."
 nasm -f bin -o build/boot.bin src/boot.asm
+nasm -f bin -o build/kernel.bin src/kernel/main.asm
 echo -e "\e[32mDone!"
 echo -n -e "\e[37mAssembling binaries..."
 dd if=/dev/zero of=wintergreen.flp count=2880 status=noxfer
+dd if=build/boot.bin of=wintergreen.flp conv=notrunc status=noxfer
+dd if=build/kernel.bin of=wintergreen.flp seek=1 conv=notrunc status=noxfer
 echo -e "\e[32mDone!"
-echo -e "\e[36mEnjoy your new OS!"
+echo -e "\e[36mEnjoy your new OS!\e[37m"
 exit 0
