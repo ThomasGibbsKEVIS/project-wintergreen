c8f980ca7770f94fe9855b7cb3b0a122a16331b3
0xF3d04a <0xF3d04a@users.noreply.github.com>
2015-12-11 13:16:56 -0500

    Finished the bootloader

--- src/boot.asm
+++ src/boot.asm
@@ -14,13 +14,35 @@
 
   ; You should have received a copy of the GNU Affero General Public License
   ; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-  mov ax, 07C0h ; Set the stack
+  mov ax, 7C0h ; Set the stack
   mov ss, ax
   mov ax, 1000h
   mov ds, ax
   mov ss, 2000h
   mov ax, 2401h ; Grant us more memory
   int 15h
-  jmp $ ; Infinite hang(temporary)
+  mov ax, 1112h ; Set up 80x50 graphics mode
+  xor bl, bl
+  int 10h
+  push dl ; Backup the disc ID
+  mov ah, 2 ; Remove that annoying cursor
+  xor bh, bh
+  mov dx, 0FA0h
+  int 10h
+  pop dl ; Restore the disc ID
+  mov ax, 207h ; Read the kernel into memory
+  mov cx, 1
+  xor dh, dh
+  xor bx, bx
+  mov es, bx
+  mov bx, 2000h
+  int 13h
+  mov ax, 2000h ; Set the stack for the kernel
+  mov ds, ax
+  mov es, ax
+  mov fs, ax
+  mov gs, ax
+  mov ss, ax
+  jmp 0h:2000h
   times 510-($-$$) db 0
   dw 0AA55h
