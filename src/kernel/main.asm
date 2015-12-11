9b7597cb33b7752eb2a9b4041491849fc842a0d3
0xF3d04a <0xF3d04a@users.noreply.github.com>
2015-12-11 15:05:15 -0500

    Updated the kernel

--- src/kernel/main.asm
+++ src/kernel/main.asm
@@ -1,7 +1,22 @@
 %include "vga.asm"
+  ; Project Wintergreen is a simple operating system made in Assembly
+  ; Copyright (C) 2015  0xF3d04a
 
+  ; This program is free software: you can redistribute it and/or modify
+  ; it under the terms of the GNU Affero General Public License as published
+  ; by the Free Software Foundation, either version 3 of the License, or
+  ; (at your option) any later version.
+
+  ; This program is distributed in the hope that it will be useful,
+  ; but WITHOUT ANY WARRANTY; without even the implied warranty of
+  ; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  ; GNU Affero General Public License for more details.
+
+  ; You should have received a copy of the GNU Affero General Public License
+  ; along with this program.  If not, see <http://www.gnu.org/licenses/>.
   BITS 16
   call init_graphics
-  mov si, copyright
+  mov si, welcome
   call println
   jmp $
+  welcome db 'Welcome to Project Wintergreen', 0
