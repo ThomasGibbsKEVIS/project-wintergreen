a43357e6246959e760e0fd0bb19706b46ca460a8
0xF3d04a <0xF3d04a@users.noreply.github.com>
2015-12-11 18:38:32 -0500

    Created the new kernel

---
+++ src/kernel/main.asm
@@ -0,0 +1,15 @@
+  BITS 16
+  mov si, welcome
+  call temp_print
+  jmp $
+  welcome db 'Welcome to Project Wintergreen', 0
+temp_print:
+  mov ah, 0Eh
+.loop:
+  lodsb
+  cmp al, 0
+  je .done
+  int 10h
+  jmp .loop
+.done:
+  ret
