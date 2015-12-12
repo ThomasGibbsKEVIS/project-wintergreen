962afc999de7c2d45973f0c95d0c742bb781075c
0xF3d04a <0xF3d04a@users.noreply.github.com>
2015-12-12 15:12:29 -0500

    Finished most of the GDT

--- src/kernel/gdt.asm
+++ src/kernel/gdt.asm
@@ -1,8 +1,16 @@
   BITS 16
-; TODO: Write 32-bit GDT loading function
-; TODO: Write 32-bit IDT loading function
-; TODO: Write 32-bit GDT entry encoding function
-  BITS 32
-; TODO: Write 64-bit GDT loading function
-; TODO: Write 64-bit IDT loading function
-; TODO: Write 64-bit GDT entry encoding function
+load_gdt32:
+  cli
+  lgdt [gdt_desc]
+  mov eax, cr0
+  or al, 2
+  mov cr0, eax
+  ret
+gdt_32:
+  dq 0
+  dq 0C09A0000001000h
+  dq 0C0920000001000h
+  dq 0
+gdt_32_desc:
+  dw gdt_32
+  dd gdt_32 - gdt_32_desc - 1
