b99fdba64b9981abbed6bc5421ccb591417329cd
0xF3d04a <0xF3d04a@users.noreply.github.com>
2015-12-12 10:58:55 -0500

    Wrote the structure of the kernel

--- src/kernel/main.asm
+++ src/kernel/main.asm
@@ -1,15 +1,18 @@
   BITS 16
-  mov si, welcome
-  call temp_print
-  jmp $
-  welcome db 'Welcome to Project Wintergreen!', 0
-temp_print:
-  mov ah, 0Eh
-.loop:
-  lodsb
-  cmp al, 0
-  je .done
-  int 10h
-  jmp .loop
-.done:
-  ret
+bootup_16:
+  ; TODO: Enable the A20
+  ; TODO: Load the 32-bit GDT
+  ; TODO: Load the 32-bit IDT
+  ; TODO: Make the jump to 32-bit
+  BITS 32
+bootup_32:
+  ; TODO: Enable paging
+  ; TODO: Load the 64-bit GDT
+  ; TODO: Load the 64-bit IDT
+  ; TODO: Make the jump to 64-bit
+  BITS 64
+bootup_64:
+  ; TODO: Enable all the other drivers
+  ; TODO: Display the bootsplash
+routine:
+  ; TODO: Write the multitasking routine
