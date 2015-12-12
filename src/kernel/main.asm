a223bbb5ae49b285dab6d16bc7b7f18aa62653c9
0xF3d04a <0xF3d04a@users.noreply.github.com>
2015-12-12 17:22:30 -0500

    Removed IDT init call and panic bitwise seperation

--- src/kernel/main.asm
+++ src/kernel/main.asm
@@ -5,27 +5,9 @@
   BITS 16
 bootup_16:
   call enable_a20
-  jc panic16
+  jc panic
   call load_gdt32
-  call load_idt32
   jmp 0h:bootup_32
   BITS 32
 bootup_32:
-  mov si, welcome
-  call temp_print
-  jmp $
-  welcome db 'Welcome to Project Wintergreen', 0
-temp_print:
-  xor ax, ax
-  mov gs, ax
-  xor di, di
-  mov ah, 0Fh
-.loop:
-  lodsb
-  cmp al, 0
-  je .done
-  mov [gs:di], ax
-  add di, 2
-  jmp .loop
-.done:
-  ret
+  jmp panic
