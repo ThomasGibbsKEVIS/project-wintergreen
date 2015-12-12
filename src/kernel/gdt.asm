7375584c9b4cfeddc8c13109fa0190256916f1f3
0xF3d04a <0xF3d04a@users.noreply.github.com>
2015-12-12 11:54:39 -0500

    Created GDT module structure

---
+++ src/kernel/gdt.asm
@@ -0,0 +1,8 @@
+  BITS 16
+; TODO: Write 32-bit GDT loading function
+; TODO: Write 32-bit IDT loading function
+; TODO: Write 32-bit GDT entry encoding function
+  BITS 32
+; TODO: Write 64-bit GDT loading function
+; TODO: Write 64-bit IDT loading function
+; TODO: Write 64-bit GDT entry encoding function
