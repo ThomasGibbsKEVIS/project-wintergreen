aa32f3aaf20da618bbcc656f5c078a8420cbdace
0xF3d04a <0xF3d04a@users.noreply.github.com>
2015-12-12 17:38:08 -0500

    Made panic a part of the main kernel

--- src/kernel/main.asm
+++ src/kernel/main.asm
@@ -1,13 +1,12 @@
 %include "a20.asm"
 %include "gdt.asm"
-%include "panic.asm"
 %include "vga.asm"
   BITS 16
 bootup_16:
   call enable_a20
-  jc panic
+  jc $
   call load_gdt32
   jmp 0h:bootup_32
   BITS 32
 bootup_32:
-  jmp panic
+  jmp $
