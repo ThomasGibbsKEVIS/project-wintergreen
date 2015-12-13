0b0dfa3a561530b2e160b8d6a60ef712fad25b75
0xF3d04a <0xF3d04a@users.noreply.github.com>
2015-12-13 10:42:12 -0500

    Added x86 bootloader structure

---
+++ src/arch/x86/boot.asm
@@ -0,0 +1,7 @@
+BITS 16
+; TODO: Set the stack
+; TODO: Load the first 8 sectors of the filesystem
+; TODO: Find the kernel
+; TODO: Load the kernel at 0x00000000
+; TODO: Load into protected mode(32-bit)
+; TODO: Run the kernel
