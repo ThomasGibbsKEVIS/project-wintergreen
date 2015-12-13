a7b882885d188d7a5e9ec2b20988d64ee45a1fec
0xF3d04a <0xF3d04a@users.noreply.github.com>
2015-12-13 17:31:51 -0500

    Finished the getinput function

--- src/arch/x86/kernel/main.asm
+++ src/arch/x86/kernel/main.asm
@@ -9,3 +9,40 @@ routine:
   call getinput
   call multitask_routine
   jmp routine
+render:
+  ; TODO: Make a CLI
+  ret
+getinput:
+  call getkey
+  cmp ax, 0
+  je .return
+  mov bl, [linelen]
+  cmp al, 8
+  je .bs
+  cmp al, 13
+  je .send
+  cmp bl, 63
+  je .return
+  mov di, line
+  mov [di:bl], al
+  inc bl
+  mov [linelen], bl
+  jmp .return
+.bs:
+  cmp bl, 0
+  je .return
+  dec bl
+  mov [di:bl], 0
+  jmp .return
+.done:
+  mov bl, [lineready]
+  mov bl, 1
+  mov [lineready], bl
+.return:
+  ret
+multitask_routine:
+  ; TODO: Make the multitasking routine
+  ret
+line times 64 db 0
+linelen db 0
+lineready db 0
