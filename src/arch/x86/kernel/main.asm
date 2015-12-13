1f4b19d13518a51a4073495ecedd704151450237
0xF3d04a <0xF3d04a@users.noreply.github.com>
2015-12-13 17:44:32 -0500

    Created graphical environment

--- src/arch/x86/kernel/main.asm
+++ src/arch/x86/kernel/main.asm
@@ -1,48 +1,15 @@
-%include "keyboard.asm"
-%include "spearmint.asm"
-%include "starfruit.asm"
+%include "mouse.asm"
 %include "vga.asm"
 
 BITS 16
+  call init_vga
+  mov al, 15
+drawbackground:
+  mov [gs:di], al
+  inc di
+  cmp di, 64001
+  je routine
+  jmp drawbackground
 routine:
-  call render
-  call getinput
-  call multitask_routine
+  ; TODO: Render the mouse on the screen
   jmp routine
-render:
-  ; TODO: Make a CLI
-  ret
-getinput:
-  call getkey
-  cmp ax, 0
-  je .return
-  mov bl, [linelen]
-  cmp al, 8
-  je .bs
-  cmp al, 13
-  je .send
-  cmp bl, 63
-  je .return
-  mov di, line
-  mov [di:bl], al
-  inc bl
-  mov [linelen], bl
-  jmp .return
-.bs:
-  cmp bl, 0
-  je .return
-  dec bl
-  mov [di:bl], 0
-  jmp .return
-.done:
-  mov bl, [lineready]
-  mov bl, 1
-  mov [lineready], bl
-.return:
-  ret
-multitask_routine:
-  ; TODO: Make the multitasking routine
-  ret
-line times 64 db 0
-linelen db 0
-lineready db 0
