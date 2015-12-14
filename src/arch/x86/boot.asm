e2e6abcaa074a11afbe012c1871f8cf4bbc8cff4
0xF3d04a <0xF3d04a@users.noreply.github.com>
2015-12-13 20:55:13 -0500

    Created new bootloader

---
+++ src/arch/x86/boot.asm
@@ -0,0 +1,29 @@
+BITS 16
+  mov ax, 7C0h
+  mov ss, ax
+  mov ax, 1000h
+  mov ds, ax
+  mov sp, 4096
+  mov cx, 2
+  mov bx, 1000h
+  mov es, bx
+  xor bx, bx
+  xor dh, dh
+loadkernel:
+  mov ax, 208h
+  int 13h
+  jc .reset
+  jmp runkernel
+.reset:
+  xor ah, ah
+  int 13h
+  jc panic
+  jmp loadkernel
+runkernel:
+  mov ax, 1000h
+  mov ds, ax
+  mov es, ax
+  mov fs, ax
+  mov gs, ax
+  mov ss, ax
+  jmp 1000h:0h
