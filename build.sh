1cd19ecf7aaff1a8048c177826bef78629eb8a3d
0xF3d04a <0xF3d04a@users.noreply.github.com>
2015-12-14 08:33:02 -0500

    Created build script

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ ! -d build ]
+then
+  echo -n -e "\e[37mCreating build folder..."
+  mkdir build
+  echo -e "\e[32mDone!"
+fi
+if [ -z ${1} ]
+then
+  echo -e "\e[37mNo architecture specified!"
+  exit 1
+fi
+if [ -d src/arch/${1}/build.sh ]
+then
+  echo -e "\e[37mBuilding for \e[36m${1}\e[37m."
+else
+  echo -e "\e[37mInvalid architecture!"
+fi
+src/arch/${1}/build.sh
+exit 0
