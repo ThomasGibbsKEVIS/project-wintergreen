28cc36a39245741330401c8ffb47c699c9c015ad
0xF3d04a <0xF3d04a@users.noreply.github.com>
2015-12-13 19:00:25 -0500

    Finished the prototype desktop env.

--- src/arch/x86/kernel/main.asm
+++ src/arch/x86/kernel/main.asm
@@ -3,13 +3,57 @@
 
 BITS 16
   call init_vga
+  call init_mouse
   mov al, 15
 drawbackground:
   mov [gs:di], al
   inc di
   cmp di, 64001
-  je routine
+  je .pre
   jmp drawbackground
+.pre:
+  xor di, di
 routine:
-  ; TODO: Render the mouse on the screen
+  call get_mouse
+  call set_pos
+  call render
   jmp routine
+set_pos:
+  call .setx
+  call .sety
+  ret
+.setx:
+  test cl, cl
+  js .subx
+  xor ch, ch
+  add cx, [mouse_x]
+  cmp cx, 320
+  jg .maxx
+  mov [mouse_x], cx
+  ret
+.maxx:
+  mov cx, 320
+  mov [mouse_x], cx
+  ret
+.subx:
+  and cl, 127
+  xor ch, ch
+  sub cx, [mouse_x]
+  cmp cx, 127
+  jl .minx
+  mov [mouse_x], cx
+  ret
+.minx:
+  xor cx, cx
+  mov [mouse_x], cx
+  ret
+.sety:
+  ret ; Placeholder
+render:
+  mov ax, 0FF0h
+  mov [gs:di], ah
+  mov cx, [mouse_x]
+  mov di, cx
+  mov [gs:di], al
+  mouse_x dw 0
+  mouse_y db 0
